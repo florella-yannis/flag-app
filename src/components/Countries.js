39bd8869154206b15658bf1aba816290c7645977
florella-yannis <florellayannis@gmail.com>
2023-10-19 16:38:04 +0200

    mise en place useeffect pour axios

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Countries = () => {
+
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+    axios
+        .get("https://restcountries.com/v3.1/all")
+        .then((res) => console.log(res.data));
+    }, []);
+
     return (
-        <div className="countries">
-            <h1>Countries</h1>
-        </div>
-       
+    <div className="countries">
+        <h1>Countries</h1>
+    </div>
     );
 };
 
