5443b611707ecc367cd737b52952e914cc3157bd
florella-yannis <florellayannis@gmail.com>
2023-10-19 18:47:27 +0200

    ajout card + input radio + input range

---
+++ src/components/Card.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+// {country} correspond a props.country
+const Card = ({country}) => {
+    // console.log(country);
+    return (
+        
+        <li className="card">
+            <img src={country.flags.svg} alt={"drapeau " + country.translations.fra.common} />
+            <div className="infos">
+                <h2>{country.translations.fra.common}</h2>
+                <h2>{country.capital}</h2>
+                <p>Pop. {country.population.toLocaleString()}</p>
+            </div>
+        </li>
+    );
+};
+
+export default Card;
--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,21 +1,58 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import Card from "./Card";
 
 const Countries = () => {
+  const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+  const [selectedRadio, setSelectedRadio] = useState("");
 
-    const [data, setData] = useState([]);
-
-    useEffect(() => {
+  useEffect(() => {
     axios
-        .get("https://restcountries.com/v3.1/all")
-        .then((res) => console.log(res.data));
-    }, []);
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  }, []);
 
-    return (
+  return (
     <div className="countries">
-        <h1>Countries</h1>
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="continent"
+              checked={continent === selectedRadio}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
+        ))}
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
+      <ul>
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card key={index} country={country} />
+          ))}
+      </ul>
     </div>
-    );
+  );
 };
 
 export default Countries;
